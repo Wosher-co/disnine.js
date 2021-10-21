b879f169922b53868e5148d11c566bb79274e137
PolPtoAmo <pvallverdualcaide@gmail.com>
2021-10-21 18:56:18 +0200

    Created some Getters

--- src/DisBot.ts
+++ src/DisBot.ts
@@ -1,4 +1,4 @@
-import { Client, ClientOptions } from "discord.js";
+import { ChannelManager, Client, ClientOptions, ClientUser, ClientVoiceManager, GuildManager, ShardClientUtil, User, UserManager } from "discord.js";
 import CommandHandler, { CommandHandlerOptions } from "./handlers/CommandHandler";
 import EventHandler, { EventHandlerOptions } from "./handlers/EventHandler";
 
@@ -29,5 +29,34 @@ export default class DisBot {
         await this.commandManager.sendCommands(guild.id);
       });
     });
+  }
+
+  get guilds(): GuildManager {
+    return this._client.guilds
+  }
+
+  get users(): UserManager {
+    return this._client.users;
+  }
+
+  get user(): ClientUser | null {
+    return this._client.user;
+  }
+
+  get channels(): ChannelManager {
+    return this._client.channels;
+  }
+
+  get voice(): ClientVoiceManager {
+    return this._client.voice;
+  }
+
+  /** @deprecated Not supported */
+  get shard(): ShardClientUtil | null {
+    return this._client.shard;
+  }
+
+  get ready(): boolean {
+    return this._client.isReady();
   }
 }
