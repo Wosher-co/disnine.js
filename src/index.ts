2a67f21f111306e4fd7db575c3c09dcc1155ddfb
PolPtoAmo <pvallverdualcaide@gmail.com>
2021-10-22 18:36:30 +0200

    Better index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,18 @@ import DisBot from "./DisBot";
 import { BaseCommand, BaseSubcommand } from "./handlers/CommandHandler";
 import { BaseListener } from "./handlers/EventHandler";
 
-export default {
-  DisBot: DisBot,
-  BaseCommand: BaseCommand,
-  BaseSubcommand: BaseSubcommand,
-  BaseListener: BaseListener,
-}
+const modules = {
+  DisBot,
+  BaseCommand,
+  BaseSubcommand,
+  BaseListener,
+};
+
+export default modules;
+
+export {
+  DisBot,
+  BaseCommand,
+  BaseSubcommand,
+  BaseListener,
+};
