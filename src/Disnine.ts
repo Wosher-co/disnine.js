fc6abd9379a3e1d65df6960e61bf51d5f0f8ca25
PolPtoAmo <pvallverdualcaide@gmail.com>
2021-10-22 17:22:29 +0200

    Main file done

---
+++ src/Disnine.ts
@@ -0,0 +1,10 @@
+import DisBot from "./DisBot";
+import { BaseCommand, BaseSubcommand } from "./handlers/CommandHandler";
+import { BaseListener } from "./handlers/EventHandler";
+
+export default {
+  DisBot: DisBot,
+  BaseCommand: BaseCommand,
+  BaseSubcommand: BaseSubcommand,
+  BaseListener: BaseListener,
+}
